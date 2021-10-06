5fb1242c27c6b679b7265f6163a295279bead4a1
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-06 20:07:10 -0300

    Creada hoja CSS para el componente layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,9 +1,24 @@
 import React from "react";
+import './Layout.css';
 
 class Layout extends React.Component {
     render() {
         return (
-            <p>Hola, mundo!</p>
+          <div className="layout">
+            <h1>Layout</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              faucibus pellentesque felis non ornare. Maecenas pharetra nibh id
+              odio fermentum, a consectetur magna aliquam. Proin feugiat eros
+              dapibus eros rhoncus imperdiet. Sed vehicula cursus commodo. Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit. Duis tellus
+              justo, egestas id diam at, consequat ultricies ligula. Praesent
+              rutrum ipsum vestibulum, mattis dui eu, ullamcorper augue. Vivamus
+              pulvinar blandit bibendum. In tincidunt efficitur erat vel
+              imperdiet. Mauris pharetra ultricies viverra. Quisque vestibulum
+              fermentum nibh ut elementum.
+            </p>
+          </div>
         );
     }
 }
